f79f33c5a5e0d8082282c50b85869ca637468970
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-10-03 21:42:23 -0600

    3.13-14 Nodemon, Concurrently and ENV setup complete

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const products = require('./data/products');
 
+dotenv.config();
 const app = express();
 
 app.get('/api/products', (req, res) => {
@@ -12,4 +14,7 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+const MODE = process.env.NODE_ENV;
+
+app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`));
