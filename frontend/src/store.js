9730f8a3901b46510833f34980ebf93df9e6e32e
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-10-04 14:11:36 -0600

    5.27 Reducer and action for productList

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({});
+// reducer imports
+import { productListReducer } from './reducers/productReducers';
+
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 const middleware = [thunk];
 
 const initialState = {};
