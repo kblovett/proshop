0bc6a35c68a86548baf84a91e179d179d87a44db
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-10-04 21:14:02 -0600

    6.33 Add to cart functionality

--- frontend/src/components/screens/CartScreen.js
+++ frontend/src/components/screens/CartScreen.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-const CartScreen = () => {
-  return <div>Hello!!</div>;
+// bootstrap imports
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+
+// component imports
+import { Message } from 'components/common';
+
+// action imports
+import { addToCart } from 'actions';
+
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return (
+    <div>
+      {cartItems.map((item) => (
+        <p>Item: {item.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export { CartScreen };
--- frontend/src/components/screens/ProductScreen.js
+++ frontend/src/components/screens/ProductScreen.js
@@ -21,7 +21,7 @@ import { Rating, Loader, Message } from 'components/common';
 import { listProductDetails } from '../../actions/productActions';
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
